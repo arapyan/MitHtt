d07b63fdfce2d240de1d2c2e9bde712382f875fa
arapyan <>
2012-05-10 11:14:09 +0000

    bug fix-both decays now are leptonic for emu

--- Ntupler/src/TSVfitter.cc
+++ Ntupler/src/TSVfitter.cc
@@ -23,9 +23,50 @@ TLorentzVector mithep::TSVfitter::fit(mithep::TSVfit* fit, double iMet, double i
 
   // setup lepton types (depending on mass of the input particles)
   NSVfitStandalone::kDecayType lId2 = NSVfitStandalone::kLepDecay; 
-  if(fit->daughter2.M() != 0.105658 || fit->daughter2.M() != 0.000510999){  
-    lId2 = NSVfitStandalone::kHadDecay;
-  }
+  if(fit->daughter2.M() != 0.105658 || fit->daughter2.M() != 0.000510999)
+    {  
+      lId2 = NSVfitStandalone::kHadDecay;
+    }
+  // setup MET vector
+  TVector3 buffer; buffer.SetPtThetaPhi(iMet,0,iMetPhi);
+  NSVfitStandalone::Vector lMet(buffer.x(), buffer.y(), buffer.z());
+  // configure the fit
+  std::vector<NSVfitStandalone::MeasuredTauLepton> measuredTauLeptons;
+  measuredTauLeptons.push_back(NSVfitStandalone::MeasuredTauLepton(NSVfitStandalone::kLepDecay, lLep1));
+  measuredTauLeptons.push_back(NSVfitStandalone::MeasuredTauLepton(lId2                       , lLep2));
+  NSVfitStandaloneAlgorithm algo(measuredTauLeptons,lMet, lMM, 1);
+  algo.maxObjFunctionCalls(5000);
+  algo.fit();
+  NSVfitStandalone::LorentzVector lL  = algo.fittedDiTauSystem(); TLorentzVector result;
+  result.SetXYZM(lL.x(), lL.y(), lL.z(), lL.mass());
+  //Fill Additional variables
+  fMassUnc      = algo.massUncert();
+  fstatus = algo.isValidSolution();
+  //fFittedMET    = algo.fittedMET().Pt();
+  //fFittedMETPhi = algo.fittedMET().Phi();
+  //fMeasMET      = algo.measuredMET().Pt();
+  //fMeasMETPhi   = algo.measuredMET().Phi();
+  //std::cout << "===> Fit check " << fit->mass << " -- " << lL.M() << std::endl;
+  return result;
+} 
+double  mithep::TSVfitter::integrate(mithep::TSVfit* fit, double iMet, double iMetPhi) 
+{
+  // setup significance matrix
+  TMatrixD lMM(2,2); 
+  lMM(0,0) = fit->cov_00; 
+  lMM(0,1) = fit->cov_01; 
+  lMM(1,0) = fit->cov_10; 
+  lMM(1,1) = fit->cov_11; 
+  // setup lorentz vectors for input leptons
+  NSVfitStandalone::LorentzVector lLep1; lLep1.SetPxPyPzE( fit->daughter1.Px(), fit->daughter1.Py(), fit->daughter1.Pz(), fit->daughter1.E() );
+  NSVfitStandalone::LorentzVector lLep2; lLep2.SetPxPyPzE( fit->daughter2.Px(), fit->daughter2.Py(), fit->daughter2.Pz(), fit->daughter2.E() );
+  //std::cout << "---> Mass Check " << fit->daughter1.M() << " -- " << fit->daughter2.M() << std::endl;
+
+  // setup lepton types (depending on mass of the input particles)
+  NSVfitStandalone::kDecayType lId2 = NSVfitStandalone::kLepDecay; 
+  //if(fit->daughter2.M() != 0.105658 || fit->daughter2.M() != 0.000510999){  
+  //  lId2 = NSVfitStandalone::kHadDecay;
+  //}
   // setup MET vector
   TVector3 buffer; buffer.SetPtThetaPhi(iMet,0,iMetPhi);
   NSVfitStandalone::Vector lMet(buffer.x(), buffer.y(), buffer.z());
@@ -34,16 +75,8 @@ TLorentzVector mithep::TSVfitter::fit(mithep::TSVfit* fit, double iMet, double i
   measuredTauLeptons.push_back(NSVfitStandalone::MeasuredTauLepton(NSVfitStandalone::kLepDecay, lLep1));
   measuredTauLeptons.push_back(NSVfitStandalone::MeasuredTauLepton(lId2                       , lLep2));
   NSVfitStandaloneAlgorithm algo(measuredTauLeptons,lMet, lMM, 0);
-  algo.maxObjFunctionCalls(5000);
-  algo.fit(); 
-  NSVfitStandalone::LorentzVector lL  = algo.fittedDiTauSystem(); TLorentzVector result;
-  result.SetXYZM(lL.x(), lL.y(), lL.z(), lL.mass());
-  //Fill Additional variables
-  fMassUnc      = algo.massUncert();
-  //fFittedMET    = algo.fittedMET().Pt();
-  //fFittedMETPhi = algo.fittedMET().Phi();
-  //fMeasMET      = algo.measuredMET().Pt();
-  //fMeasMETPhi   = algo.measuredMET().Phi();
-  //std::cout << "===> Fit check " << fit->mass << " -- " << lL.M() << std::endl;
-  return result;
+  algo.addLogM(0);
+  algo.adddelta(1);
+  algo.integrate();
+  return algo.getMass();
 } 
