62145f125e2474f3b952d9e0bcf14c9072d1164a
vdutta <>
2013-08-05 14:58:00 +0000

    store nPU as float

--- Efficiency/EffData.hh
+++ Efficiency/EffData.hh
@@ -5,11 +5,13 @@ struct EffData
 {
   Float_t mass, pt, eta, phi, weight;
   Int_t q;
-  UInt_t npv, npu, pass;
+  UInt_t npv;
+  Float_t npu;
+  UInt_t pass;
   UInt_t runNum, lumiSec, evtNum;
   Float_t rho;
 };
 
-// "mass/F:pt:eta:phi:weight:q/I:npv/i:npu:pass:runNum:lumiSec:evtNum:rho/F"
+// "mass/F:pt:eta:phi:weight:q/I:npv/i:npu/F:pass/i:runNum:lumiSec:evtNum:rho/F"
 
 #endif
